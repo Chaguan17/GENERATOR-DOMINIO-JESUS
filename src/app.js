98c73b2b2f85d6fac09d2e94a8304573c81a353f
Jesus <33575830+Chaguan17@users.noreply.github.com>
2026-01-21 10:57:44 +0000

    Funcion de generar dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dominio =[".com", ".io", ".net",".ou"]
+
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let z = 0; z < dominio.length; z++){
+          let palabra = pronoun[i] + adj[j] + noun[k] + dominio[z];
+        console.log(palabra);
+        }
+        
+          }
+        }
+      }
+    };
